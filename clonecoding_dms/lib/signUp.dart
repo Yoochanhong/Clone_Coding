b127aeacaa29f1996428f61a6ecb058ddd6cc660
데탑 유찬홍 <chanhongy6@naver.com>
2022-09-11 23:16:33 +0900

    신청 화면

---
+++ clonecoding_dms/lib/signUp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("체크")
+    );
+  }
+}
