5d47bb8cbb83f4fc8d457cd7d2f6c5372e77bb2f
데탑 유찬홍 <chanhongy6@naver.com>
2022-09-11 23:16:38 +0900

    공지 화면

---
+++ clonecoding_dms/lib/announcement.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Announcement extends StatelessWidget {
+  const Announcement({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("공지"),
+    );
+  }
+}
