778d6e90a637dc2b92c20a40d2d5d51124cce877
데탑 유찬홍 <chanhongy6@naver.com>
2022-09-11 23:16:23 +0900

    급식 화면

---
+++ clonecoding_dms/lib/mealsPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MealsPage extends StatefulWidget {
+  const MealsPage({Key? key}) : super(key: key);
+
+  @override
+  State<MealsPage> createState() => _MealsPageState();
+}
+
+class _MealsPageState extends State<MealsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
