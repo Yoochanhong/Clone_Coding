e23f3fd6209400bdc445ac9b80bf6b36702db5cf
데탑 유찬홍 <chanhongy6@naver.com>
2022-09-12 12:30:34 +0900

    신청 클래스

---
+++ clonecoding_dms/lib/signUpClass.dart
@@ -0,0 +1,26 @@
+class signUpClass {
+  final String realTitle;
+  final String title;
+  final String subText;
+  final String firstTitle;
+  final String firstSubText;
+  final String secondTitle;
+  final String secondSubText;
+  final String thirdTitle;
+  final String thirdSubText;
+  final String? fourthTitle;
+  final String? fourthSubTitle;
+
+  signUpClass(
+      {required this.realTitle,
+      required this.title,
+      required this.subText,
+      required this.firstTitle,
+      required this.firstSubText,
+      required this.secondTitle,
+      required this.secondSubText,
+      required this.thirdTitle,
+      required this.thirdSubText,
+      this.fourthTitle,
+      this.fourthSubTitle});
+}
