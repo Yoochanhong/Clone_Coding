186f7729db6855dc0264cb45afff5667bb910927
Yoochanhong <chanhongy6@naver.com>
2022-09-13 08:24:01 +0900

    Meals 버튼 추가

--- clonecoding_dms/lib/signup/button.dart
+++ clonecoding_dms/lib/signup/button.dart
@@ -39,3 +39,48 @@ Widget signupButton(String title, String subTitle) {
     ],
   );
 }
+
+
+Widget mealsButton(String title, String subTitle) {
+  return Column(
+    children: [
+      TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          overlayColor: MaterialStateColor.resolveWith(
+                  (states) => Colors.transparent), //버튼 애니메이션 삭제
+        ),
+        child: SizedBox(
+          height: 160.0,
+          width: 500.0,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              //모서리 둥글게 깎기
+                borderRadius: BorderRadius.circular(20.0)),
+            elevation: 2.8,//그림자 깊이
+            shadowColor: Colors.teal[200],
+            child: Center(
+              child: ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    '$title',
+                    style: TextStyle(
+                        color: Colors.teal[400],
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 3.0, 0.0, 40.0),
+                  child: Text('$subTitle'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
