35f3d9ff9ccdc05e45eb2a4cbc0f52a102ad4be2
Yoochanhong <chanhongy6@naver.com>
2022-09-12 23:30:17 +0900

    signUp버튼 추가

---
+++ clonecoding_dms/lib/signup/button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+Widget signupButton(String title, String subTitle) {
+  return Column(
+    children: [
+      TextButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          overlayColor: MaterialStateColor.resolveWith(
+                  (states) => Colors.transparent), //버튼 애니메이션 삭제
+        ),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            //모서리 둥글게 깎기
+              borderRadius: BorderRadius.circular(20.0)),
+          elevation: 2.8,//그림자 깊이
+          shadowColor: Colors.teal[200],
+          child: Center(
+            child: ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(left: 10.0, top: 40.0),
+                child: Text(
+                  '$title',
+                  style: TextStyle(
+                    color: Colors.teal[400],
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 3.0, 0.0, 40.0),
+                child: Text('$subTitle'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
