e1a78d088a2c1410ae1133fb2ee7514c09815f10
Yoochanhong <chanhongy6@naver.com>
2022-09-13 20:10:26 +0900

    신청 클래스

---
+++ clonecoding_dms/lib/signup/signUpClass.dart
@@ -0,0 +1,26 @@
+class SignUpClass {
+  String realTitle;
+  String title;
+  String subText;
+  String firstTitle;
+  String firstSubText;
+  String secondTitle;
+  String secondSubText;
+  String thirdTitle;
+  String thirdSubText;
+  String fourthTitle;
+  String fourthSubTitle;
+
+  SignUpClass(
+      this.realTitle,
+      this.title,
+      this.subText,
+      this.firstTitle,
+      this.firstSubText,
+      this.secondTitle,
+      this.secondSubText,
+      this.thirdTitle,
+      this.thirdSubText,
+      this.fourthTitle,
+      this.fourthSubTitle);
+}
