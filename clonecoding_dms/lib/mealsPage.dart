df7fcb36c8c6d909bc6d42954a3c47c1b2b58ea7
Yoochanhong <chanhongy6@naver.com>
2022-09-18 15:13:03 +0900

    pageView, pageViewConrollButton 전환

--- clonecoding_dms/lib/mealsPage.dart
+++ clonecoding_dms/lib/mealsPage.dart
@@ -1,6 +1,8 @@
 import 'package:clonecoding_dms/button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class MealsPage extends StatefulWidget {
   const MealsPage({Key? key}) : super(key: key);
@@ -12,38 +14,60 @@ class MealsPage extends StatefulWidget {
 class _MealsPageState extends State<MealsPage>
     with SingleTickerProviderStateMixin {
   //상태가 잆는 위젯은 상태값들을 보존하고 추적할 수 있다.
-  TabController? _tabController;
 
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
+  PageController page = PageController(initialPage: 5);
+  int pageIndex = 5;
 
   @override
   Widget build(BuildContext context) {
+    String getSystemTime() {
+      var now = DateTime.now();
+      return DateFormat("yyyy년 MM월 dd일").format(now);
+    }
+
     return SafeArea(
       child: Container(
         child: Center(
           child: Column(
             children: [
-              TabBar(
-                indicatorColor: Colors.transparent,
-                controller: _tabController,
-                tabs: [
-                  Tab(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      page.animateToPage(--pageIndex,
+                          duration: Duration(milliseconds: 400),
+                          curve: Curves.linearToEaseOut);
+                    },
                     icon: Icon(
-                      Icons.chevron_left,
-                      color: Colors.black,
+                      Icons.keyboard_arrow_left_rounded,
+                      size: 100.0.sp,
                     ),
                   ),
-                  Text(
-                    '2022년 9월 12일\n         월요일',
-                    style: TextStyle(color: Colors.black, fontSize: 35.w),
+                  Column(
+                    children: [
+                      TimerBuilder.periodic(
+                        (Duration(days: 1)),
+                        builder: (context) {
+                          return Text(
+                            '${getSystemTime()}',
+                            style: TextStyle(
+                              fontSize: 40.0.sp,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  Tab(
+                  IconButton(
+                    onPressed: () {
+                      page.animateToPage(++pageIndex,
+                          duration: Duration(milliseconds: 400),
+                          curve: Curves.linearToEaseOut);
+                    },
                     icon: Icon(
-                      Icons.chevron_right,
-                      color: Colors.black,
+                      Icons.keyboard_arrow_right_rounded,
+                      size: 100.0.sp,
                     ),
                   ),
                 ],
@@ -53,15 +77,21 @@ class _MealsPageState extends State<MealsPage>
                 child: SizedBox(
                   width: 1000.0.w,
                   height: 1400.0.h,
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        mealsButton('아침', '급식이 없습니다.'),
-                        mealsButton('점심', '급식이 없습니다.'),
-                        mealsButton('저녁', '급식이 없습니다.'),
-                      ],
-                    ),
+                  child: PageView.builder(
+                    controller: page,
+                    itemCount: 11, //페이지 수
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            mealsButton('아침', '급식이 없습니다.'),
+                            mealsButton('점심', '급식이 없습니다.'),
+                            mealsButton('저녁', '급식이 없습니다.'),
+                          ],
+                        ),
+                      );
+                    }, //page 의 반목문 항목 형성
                   ),
                 ),
               ),
