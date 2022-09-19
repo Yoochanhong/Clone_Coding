fb7f9e87ded015640757f960c2e108dad9c386f8
Yoochanhong <chanhongy6@naver.com>
2022-09-19 21:54:03 +0900

    연, 월, 일, 요일 구현

--- clonecoding_dms/lib/mealsPage.dart
+++ clonecoding_dms/lib/mealsPage.dart
@@ -15,16 +15,15 @@ class _MealsPageState extends State<MealsPage>
     with SingleTickerProviderStateMixin {
   //상태가 잆는 위젯은 상태값들을 보존하고 추적할 수 있다.
 
-  PageController page = PageController(initialPage: 5);
-  int pageIndex = 5;
-
+  PageController page = PageController(initialPage: 3);
+  int pageIndex = 3;
+  DateTime date = DateTime.now();
+  String getSystemTime(){
+    var now = DateTime.now();
+    return DateFormat("yyy년 MM월 d일").format(now);
+  }
   @override
   Widget build(BuildContext context) {
-    String getSystemTime() {
-      var now = DateTime.now();
-      return DateFormat("yyyy년 MM월 dd일").format(now);
-    }
-
     return SafeArea(
       child: Container(
         child: Center(
@@ -48,15 +47,14 @@ class _MealsPageState extends State<MealsPage>
                     children: [
                       TimerBuilder.periodic(
                         (Duration(days: 1)),
-                        builder: (context) {
+                        builder: (context){
+                          print('${getSystemTime()}');
                           return Text(
                             '${getSystemTime()}',
-                            style: TextStyle(
-                              fontSize: 40.0.sp,
-                            ),
                           );
                         },
                       ),
+                      Text(DateFormat(' EEEE ').format(date)),
                     ],
                   ),
                   IconButton(
