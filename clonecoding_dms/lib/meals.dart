f1b9482a6187d8799b5e25c88a832c1e2c6f6f46
Yoochanhong <chanhongy6@naver.com>
2022-09-26 22:46:43 +0900

    Meals 클래스 다시 만들기

--- clonecoding_dms/lib/meals.dart
+++ clonecoding_dms/lib/meals.dart
@@ -1,6 +1,15 @@
-class Meals{
-  String breakfast;
-  String lunch;
-  String dinner;
-  Meals(this.breakfast, this.lunch, this.dinner);
+class Meals {
+  String? breakfast;
+  String? lunch;
+  String? dinner;
+
+  Meals({this.breakfast, this.lunch, this.dinner});
+
+  factory Meals.fromjson(Map<dynamic, dynamic> json) {
+    return Meals(
+      breakfast: json['breakfast'],
+      lunch: json['lunch'],
+      dinner: json['dinner'],
+    );
+  }
 }
