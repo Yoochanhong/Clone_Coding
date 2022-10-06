1cceb44450028765c73e77c3ecd5cbd0c03d3363
Yoochanhong <chanhongy6@naver.com>
2022-10-06 21:32:11 +0900

    getMeals 다시 만들기

--- clonecoding_dms/lib/getMeals.dart
+++ clonecoding_dms/lib/getMeals.dart
@@ -1,17 +1,14 @@
-import 'dart:convert';
 import 'package:clonecoding_dms/meals.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<Meals> getMeals() async {
-  int year = DateTime.now().year;
-  int month = DateTime.now().month;
-  int day = DateTime.now().day;
-  final response =
-  await http.get(Uri.parse('https://api.dsm-dms.com/meal/$year-$month-$day'));
+  final response = await http
+      .get(Uri.parse('https://api.dsm-dms.com/meal/${getSystemTime()}'));
   if (response.statusCode == 200) {
     print(json.decode(utf8.decode(response.bodyBytes)));
-    return Meals.fromjson(json.decode(utf8.decode(response.bodyBytes)));
+    return Meals.fromJson(json.decode(utf8.decode(response.bodyBytes)));
   } else {
-    throw Exception('실패');
+    throw Exception('예외처리');
   }
 }
