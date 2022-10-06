3af7dc9ad951ba3513425de03e058a41a6efbd28
Yoochanhong <chanhongy6@naver.com>
2022-10-06 21:27:44 +0900

    Meals 클래스 생성

--- clonecoding_dms/lib/meals.dart
+++ clonecoding_dms/lib/meals.dart
@@ -1,15 +1,46 @@
-class Meals {
-  String? breakfast;
-  String? lunch;
-  String? dinner;
+import 'package:intl/intl.dart';
 
-  Meals({this.breakfast, this.lunch, this.dinner});
+String getSystemTime() {
+  var now = DateTime.now();
+  return DateFormat("yyy-MM-dd").format(now);
+}
+class Test {
+  Date? date;
 
-  factory Meals.fromjson(Map<dynamic, dynamic> json) {
-    return Meals(
-      breakfast: json['breakfast'],
-      lunch: json['lunch'],
-      dinner: json['dinner'],
-    );
+  Test({this.date});
+
+  Test.fromJson(Map<String, dynamic> json) {
+    date = json['${getSystemTime()}'] != null ? new Date.fromJson(json['${getSystemTime()}']) : null;
+    print('${getSystemTime()}');
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.date != null) {
+      data['${getSystemTime()}'] = this.date!.toJson();
+    }
+    return data;
+  }
+}
+
+class Date {
+  List<String>? breakfast;
+  List<String>? dinner;
+  List<String>? lunch;
+
+  Date({this.breakfast, this.dinner, this.lunch});
+
+  Date.fromJson(Map<String, dynamic> json) {
+    breakfast = json['breakfast'].cast<String>();
+    dinner = json['dinner'].cast<String>();
+    lunch = json['lunch'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['breakfast'] = this.breakfast;
+    data['dinner'] = this.dinner;
+    data['lunch'] = this.lunch;
+    return data;
   }
 }
