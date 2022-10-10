a77cba7ced079852998135d54737367c9a9921f7
Yoochanhong <chanhongy6@naver.com>
2022-10-10 16:09:59 +0900

    로그인 페이지 Scaffold 구현

--- clonecoding_dms/lib/singIn.dart
+++ clonecoding_dms/lib/singIn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
@@ -11,8 +12,36 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal[400],
       body: Container(
-        color: Colors.teal[400],
+        child: Padding(
+          padding: EdgeInsets.all(140.0.r),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/dmsLogo.png',
+                width: 150.0.w,
+                height: 150.0.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50.0.h, bottom: 20.0.h),
+                child: Text(
+                  'DMS for Flutter',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Text('Dormitory Management System',
+              style: TextStyle(
+                color: Colors.white,
+              ),),
+            ],
+          ),
+        ),
       ),
     );
   }
