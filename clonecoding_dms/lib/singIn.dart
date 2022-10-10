677b0d1acd4e05fcf0bfaa8ea36493dea09434c8
Yoochanhong <chanhongy6@naver.com>
2022-10-10 16:54:42 +0900

    둥근 Container 위젯 추가

--- clonecoding_dms/lib/singIn.dart
+++ clonecoding_dms/lib/singIn.dart
@@ -13,35 +13,57 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.teal[400],
-      body: Container(
-        child: Padding(
-          padding: EdgeInsets.all(140.0.r),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/dmsLogo.png',
-                width: 150.0.w,
-                height: 150.0.h,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 50.0.h, bottom: 20.0.h),
-                child: Text(
-                  'DMS for Flutter',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            child: Padding(
+              padding:
+                  EdgeInsets.only(top: 140.0.h, left: 140.0.w, bottom: 90.0.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/dmsLogo.png',
+                    width: 150.0.w,
+                    height: 150.0.h,
                   ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 50.0.h, bottom: 20.0.h),
+                    child: Text(
+                      'DMS for Flutter',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Dormitory Management System',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 11,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 1080.0.w,
+            height: 1380.7.h,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0.r),
+                  topRight: Radius.circular(50.0.r),
                 ),
               ),
-              Text('Dormitory Management System',
-              style: TextStyle(
-                color: Colors.white,
-              ),),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
