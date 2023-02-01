8c2948a688bf238f4d831d238caf278848816215
맥북 유찬홍 <chanhongy6@naver.com>
2023-02-02 02:53:06 +0900

    배경색 변경

--- Google/lib/main.dart
+++ Google/lib/main.dart
@@ -20,6 +20,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff202325),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
