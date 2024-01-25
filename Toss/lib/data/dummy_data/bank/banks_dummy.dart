1d43fcea5c14757a4e39558a0896ca9fd249996c
yoochanhong <chanhongy6@naver.com>
2024-01-26 01:39:01 +0900

    은행 더미데이터

---
+++ Toss/lib/data/dummy_data/bank/banks_dummy.dart
@@ -0,0 +1,5 @@
+import 'package:toss/domain/entity/bank/bank.dart';
+
+const bankShinhan = Bank(name: '신한은행', logoImagePath: '/bank/bank_shinhan.png');
+const bankKakao = Bank(name: '카카오뱅크', logoImagePath: '/bank/bank_kakao.png');
+const bankToss = Bank(name: '토스뱅크', logoImagePath: '/bank/bank_toss.png');
