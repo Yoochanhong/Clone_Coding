3a4560bc7877c95eb839ee1775e1f1e9825f8c15
yoochanhong <chanhongy6@naver.com>
2024-01-26 01:39:42 +0900

    계좌 더미데이터

---
+++ Toss/lib/data/dummy_data/bank/bank_accounts_dummy.dart
@@ -0,0 +1,23 @@
+import 'package:toss/data/dummy_data/bank/banks_dummy.dart';
+import 'package:toss/domain/entity/bank/bank_account.dart';
+
+final bankAccountShinhan1 = BankAccount(
+    bank: bankShinhan, balance: 300000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+
+final bankAccountShinhan2 =
+    BankAccount(bank: bankShinhan, balance: 3000000, accountTypeName: "저축예금");
+
+final bankAccountShinhan3 =
+    BankAccount(bank: bankShinhan, balance: 300000000, accountTypeName: "저축예금");
+final bankAccountToss = BankAccount(bank: bankToss, balance: 50000000);
+
+final bankAccountKakao =
+    BankAccount(bank: bankKakao, balance: 70000000, accountTypeName: "입출금통장");
+
+final bankAccounts = [
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao,
+];
